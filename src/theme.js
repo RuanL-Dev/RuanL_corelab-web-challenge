d4930074f6bf0b703e8274d04b7cc08aed19c88a
Ruan Lopes Gomes <ruanlopes21@gmail.com>
2022-10-24 13:29:36 -0300

    Theme config

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  primary: '#81DECF',
+  secondary: 'rgba(101, 220, 199, 0.3)',
+  background: '#FFFFFF',
+  secondBackgroundColor: '#ECECEC',
+  buttonColor: 'rgba(101, 220, 199, 0.8)',
+  buttonHover: 'rgba(21, 240, 240, 0.7)',
+  error: 'red',
+  disabled: 'grey'
+}
+
+export default theme
